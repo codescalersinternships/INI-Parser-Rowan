ef1c2a0eef574a219f0715a5271bb6daddc748fc
rowanxgamal <rowangamalelkhouly@gmail.com>
2024-06-25 16:45:34 +0300

    testing github actions for now

---
+++ cmd/ini_parser/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	rowan_gamal := "rue"
+	fmt.Println("hello")
+	fmt.Println(rowan_gamal)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/codescalersinternships/INI-Parser-Rowan
+
+go 1.22.4
