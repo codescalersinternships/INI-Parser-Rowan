826a4bc5e7d6600ab174f361637e2f665d65e863
rowanxgamal <rowangamalelkhouly@gmail.com>
2024-06-25 22:58:44 +0300

    feat: implemented struct of parser and parsing function itself, exported Parser, but didn't export functions yet

--- cmd/ini_parser/main.go
+++ cmd/ini_parser/main.go
@@ -1,11 +1,23 @@
 package main
 
 import (
-	"fmt"
+// "fmt"
+// "strings"
+// "regexp"
 )
 
 func main() {
-	fmt.Println("hello")
-	loadFromFile("cmd/ini_parser/file.ini")
-	loadFromString("rowan \n says \n hi \n nom")
+	// var m map[int]int
+	// m = make(map[int]int)
+	// m[1] = 3
+	// str := "  Hello World  \n"
+	// trimmed := strings.Trim(str, " ")
+	// trimmed = trimmed[:len(trimmed) -1]
+	// trimmed = strings.Replace(trimmed,"[","", 1)
+	// trimmed = strings.Replace(trimmed,"]","", 1)
+	// fmt.Print(trimmed)
+	// fmt.Println("hello")
+	parserLogic(loadFromFile("cmd/ini_parser/file.ini"))
+
+	// loadFromString("rowan \nsays \nhi \nnom")
 }
---
+++ cmd/ini_parser/parser.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+// Parser Structure: dictionary, sections
+// It's basically a map of a map
+// map[section] --> returns a map where each key --> value
+type Parser struct {
+	dictionary map[string]map[string]string
+	sections   []string
+}
+
+func loadFromFile(fileName string) []string {
+	var iniLines []string
+	var input io.Reader
+	fmt.Println("hi")
+
+	file, err := os.Open(fileName)
+	if err != nil {
+		fmt.Println("Cannot open file!")
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	input = file
+	scanner := bufio.NewScanner(input)
+	for scanner.Scan() {
+		iniLines = append(iniLines, scanner.Text())
+	}
+	// for _, line := range iniLines {
+	// 	fmt.Println(line)
+	// }
+	return iniLines
+
+}
+
+func loadFromString(str string) []string {
+	var iniLines []string
+	iniLines = strings.Split(str, "\n")
+	// for _, line := range iniLines {
+	// 	fmt.Println(line)
+	// }
+	return iniLines
+}
+
+func parserLogic(iniLines []string) {
+	var sections []string
+	parsedMap := Parser{make(map[string]map[string]string), sections}
+
+	var section, key, value string
+
+	for _, line := range iniLines {
+		// fmt.Println(line)
+		if line[0] == '[' {
+			for j, ch := range line {
+				if ch == ']' {
+					section = line[1:j]
+					parsedMap.sections = append(parsedMap.sections, section)
+				}
+			}
+			if parsedMap.dictionary == nil {
+				parsedMap.dictionary = make(map[string]map[string]string)
+			}
+
+		} else if line[0] == ';' || line[0] == ' ' || line[0] == '\n' || line[0] == '\t' {
+			continue
+		} else {
+			for j, ch := range line {
+				if ch == '=' {
+					key = line[0:j]
+					value = line[j+1:]
+					key = strings.Trim(key, " ")
+					value = strings.Trim(value, " ")
+					if parsedMap.dictionary[section] == nil {
+						parsedMap.dictionary[section] = make(map[string]string)
+						parsedMap.dictionary[section][key] = value
+					}
+					break
+				}
+			}
+		}
+		fmt.Println(parsedMap.sections)
+		fmt.Println(parsedMap.dictionary)
+	}
+}
